0a6ab487b53a9722f1eea36200fd85482cd7a14f
Anirudh Jain <cyna@Anirudhs-MacBook-Pro.local>
2019-07-08 17:58:40 +0530

    Single Variable Linear Regression with All Columns to check Linearity

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+import numpy as np
+from sklearn import linear_model
 
-boston=pd.read_csv('train.csv')
-boston.head()
-print(boston)
-sns.set(rc={'figure.figsize':(11.7,8.27)})
-sns.distplot(boston['medv'], bins=100)
+boston = pd.read_csv('train.csv')
+boston = boston[boston.medv < 34]
+boston = boston[boston.medv > 6]
+print(boston.shape)
+boston = boston.dropna()
+
+y_train = boston['medv'].values.reshape(-1, 1)
+
+reg=linear_model.LinearRegression()
+
+boston = boston.set_index('ID')
+
+print(boston.columns)
+
+for value in boston.columns[:-1]:
+    X_train = boston[value].values.reshape(-1, 1)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_train)
+    plt.scatter(x=y_train, y=y_pred)
+    plt.xscale('linear')
+    plt.yscale('linear')
+    plt.xlabel('medv')
+    plt.ylabel('Predicted from ' + value)
+    plt.savefig("comp/" + value)
+    plt.figure()
+
 plt.show()
